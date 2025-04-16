560dc1bd1af737c0b9c2baa04a4ef1853e780281
abhay1827 <abhayrathoreyup@gmail.com>
2025-04-16 21:47:21 +0530

    watch history pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import {User} from "../models/user.model.js"
 import {uploadOnCloudinary} from '../utils/cloudinary.js'
 import {ApiResponse} from "../utils/ApiResponse.js"
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose";
 
 
 const generateAcessAndRefreshTokes = async (userId)=>{
@@ -90,7 +91,7 @@ const createdUser = await User.findById(user._id).select(
 )
 
 if(!createdUser){
-    throw new ApiError(500,"Something went wrong while creating the user in datase");
+    throw new ApiError(500,"Something went wrong while creating the user in database");
 }
 //console.log(createdUser)
 
@@ -176,7 +177,7 @@ return res.status(200)
 })
 
 const refreshAccessToken = asyncHandler(async(req,res)=>{
-  const incomingRefreshToken =  req.cookie.refreshToken ||req.body.refreshToken // first one for system || second for mobile
+  const incomingRefreshToken =  req.cookies.refreshToken ||req.body.refreshToken // first one for system || second for mobile
 
   if(!incomingRefreshToken){
     throw new ApiError(401,"Session expired")
@@ -207,7 +208,7 @@ const refreshAccessToken = asyncHandler(async(req,res)=>{
     return res
     .status(200)
     .cookie("accessToken",accessToken,options)
-    .cookie("refresh Token",newrefreshToken,options)
+    .cookie("refreshToken",newrefreshToken,options)
     .json(
        new ApiResponse(
            200,
@@ -245,7 +246,9 @@ const changeCurrentPassword = asyncHandler ( async(req,res)=>{
 const getCurrentUser = asyncHandler(async(req,res)=>{
     return res
     .status(200)
-    .json(200,req.user,"current user fetched successfully")
+    .json(new ApiResponse(
+        200,req.user,"current user fetched successfully"
+    ))
 })
  const updateAccountDetails = asyncHandler (async(req,res)=>{
     const {fullname,email} = req.body
@@ -292,7 +295,7 @@ const getCurrentUser = asyncHandler(async(req,res)=>{
 
     return res
     .status(200)
-    .json(200,user,"avatar image uploaded")
+    .json(new ApiResponse(200,user,"avatar image uploaded"))
 
  })
 
@@ -320,10 +323,134 @@ const getCurrentUser = asyncHandler(async(req,res)=>{
 
     return res
     .status(200)
-    .json(200,user,"cover image uploaded")
+    .json(new ApiResponse(
+        200,user,"cover image uploaded"
+    ))
 
  })
 
+
+ const getUserChannelProfile = asyncHandler(async(req,res)=>{
+        const{username}=req.params
+        if (!username?.trim) {
+            throw new ApiError(400,"username is missing")
+        }
+
+       const channel = await User.aggregate([
+        {
+            $match:{
+                username:username?.toLowerCase()
+            }
+        },
+        {
+            $lookup:{
+                    from : "subscription",
+                    localField:"_id",
+                    foreignField:"channel",
+                    as: "subscribers"
+            }
+        },
+        {
+            $lookup:{
+                from : "subscription",
+                localField:"_id",
+                foreignField:"subscriber",
+                as: "subscribedTo"
+            }
+        },
+        {
+            $addFields:{
+                subscribersCount:{
+                    $size:"$subscribers"
+                },
+                channelsSubscribedToCount:{
+                    $size :"$subscribedTo"
+                },
+                isSubscribed:{
+                    $cond:{
+                        if:{$in:[req.user?._id,"$subscribers.subscriber"]},
+                        then:true,
+                        else:false
+                    }
+                }
+
+            }
+        },{
+            $project:{
+                fullname:1,
+                username:1,
+                subscribersCount:1,
+                channelsSubscribedToCount:1,
+                isSubscribed:1,
+                avatar:1,
+                coverImage:1,
+                email:1
+            }
+        }
+       ])
+       if (!channel?.length) {
+        throw new ApiError(404,"channel does not exist")
+       }
+
+       return res
+       .status(200)
+       .json(
+        new ApiResponse(200,channel[0],"User channel fetched successfully")
+       )
+
+ })
+
+ const getWatchHistory = asyncHandler(async(req,res)=>{
+    const user = await User.aggregate([
+        {
+            $match:{
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },{
+            $lookup:{
+                from:"video",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullname:1,
+                                        username:1,
+                                        avatar:1
+                                    }
+                                }
+                            ]
+                        }
+                    },{
+                        $addFields:{
+                            owner:{
+                                $first :"$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "watch history fetched successfully"
+        )
+    )
+ })
 
 export  {
     registerUser,
@@ -334,5 +461,7 @@ export  {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
-}
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
+} 
